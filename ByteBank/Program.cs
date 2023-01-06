67e950adee4c3ea3e20f251b35b476cfb7801507
italo <italobaracho11@gmail.com>
2023-01-05 21:44:18 -0300

    first commit

---
+++ ByteBank/Program.cs
@@ -0,0 +1,45 @@
+﻿using ByteBank.SistemaMenu;
+using System;
+using ByteBank.Titular;
+
+
+
+
+namespace Console.ByteBank
+{ 
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var conta = new Conta();
+            conta.NomeConta = "Conta teste";
+            conta.NumeroConta = 1000;
+            conta.Agencia = 001;
+            conta.Saldo = 1000;
+
+            conta.Titular = new Titular()
+            {
+                Cpf = "32233232323",
+                Rg = "324234234232",
+                Endereço = "Rua Canario",
+                NomeConta = "Chição",
+            };
+
+            Console.WriteLine($"O saldo atual é: { conta.VerSaldo()}");
+
+            conta.Depositar(150);
+
+            Console.WriteLine($"O saldo atual, após o deposito é: { conta.VerSaldo()}");
+
+            conta.Sacar(900);
+
+            Console.WriteLine($"O saldo atual, após o saque é: { conta.VerSaldo()}");
+
+            conta.Sacar(2000);
+
+        }
+
+    }
+     
+}
---
+++ ByteBank/SistemaMenu/Conta.cs
@@ -0,0 +1,38 @@
+﻿using ByteBank.Titular;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaMenu
+{ 
+    public class Titular
+    {
+        public int Agencia { get; set; }    
+        public int NumeroConta { get; set; }
+        public int NomeConta { get; set; }
+        public Titular Nome { get: set; }
+        public Decimal Saldo { get; set; } 
+        public Decimal VerSaldo()
+        {
+            return Saldo;
+        }
+
+        public void Depositar(decimal valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Sacar(decimal valor)
+        {
+            if (valor <= Saldo)
+                Saldo -= valor;
+            else
+                Console.WriteLine("Saque não pode ser realizado, pos não tem saldo suficiente");
+        }
+
+
+
+    }
+}
